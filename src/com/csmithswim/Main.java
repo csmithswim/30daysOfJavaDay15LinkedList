2e7adfba1e230ed51d75b56e96f576ae38644cf0
Cullen S <cullensmi@gmail.com>
2020-12-18 22:29:27 -0500

    commented out objective

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -3,6 +3,18 @@ package com.csmithswim;
 import java.io.*;
 import java.util.*;
 
+/*
+Today we will work with a Linked List. Check out the Tutorial tab for learning materials and an instructional video.
+
+A Node class is provided for you in the editor. A Node object has an integer data field,
+, and a Node instance pointer,
+
+, pointing to another node (i.e.: the next node in the list).
+
+A Node insert function is also declared in your editor. It has two parameters: a pointer,
+, pointing to the first node of a linked list, and an integer, , that must be added to the end of the list as a new Node object.
+ */
+
 class Node {
     int data;
     Node next;
